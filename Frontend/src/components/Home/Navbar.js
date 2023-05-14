9a2659f067c7f56f2c145ab879c52cbfe229b9c4
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-05-14 15:36:49 +0530

    Add files via upload

---
+++ Frontend/src/components/Home/Navbar.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Popper from '@mui/material/Popper';
+import Fade from '@mui/material/Fade';
+import Paper from '@mui/material/Paper';
+
+function Navbar() {
+    let navigate=useNavigate()
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [placement, setPlacement] = React.useState();
+    const userLoginInfo=JSON.parse(sessionStorage.getItem('userLoginInfo'))
+
+    function logoutClick(){
+        sessionStorage.clear()
+        navigate('/login')
+    }
+
+    const handleClick = (newPlacement) => (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+      };
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+                <div className="container-fluid">
+                    <span className="navbar-brand" >Gmail Service</span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ marginLeft: "200px" }}>
+                        <form className="d-flex" style={{ width: '60%' }}>
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div className='row d-flex justify-content-end'>
+                        <div className='col '>
+                            <i className="fs-4 fa-regular fa-circle-question icon" title='Support'></i>
+                        </div>
+                        <div className='col'>
+                            <i className="fs-4 fa-solid fa-gear icon" title='Settings'></i>
+                        </div>
+                        <div className='col'>
+                            <i className="fs-4 fa-regular fa-circle-user icon" title='Profile' onClick={handleClick('bottom-end')}></i>
+                        </div>
+
+                        <Popper  open={open} anchorEl={anchorEl} placement={placement} transition>
+                            {({ TransitionProps }) => (
+                            <Fade {...TransitionProps} timeout={350}>
+                                <Paper>
+                                <div className='d-flex '>
+                                    <div className='mt-4 ms-3'>
+                                        <i className="fs-3 fa-regular fa-circle-user icon"></i>
+                                    </div>
+                                    <div className='mt-2 ms-2 me-3'>
+                                        <span>{userLoginInfo[0].firstName} </span><span>{userLoginInfo[0].lastName}</span> <br />
+                                        <span>{userLoginInfo[0].username}</span>
+                                    </div>
+                                </div>
+                                <div className='mt-3 text-center pb-3'>
+                                    <button className="btn btn-outline-warning" onClick={()=>{logoutClick()}}>Logout</button>
+                                </div>
+                                </Paper>
+                            </Fade>
+                            )}
+                        </Popper>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ Frontend/src/components/Signup/SignupPhoneNumber.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import { TextField } from '@mui/material'
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function SignupPhoneNumber() {
+    let navigate = useNavigate()
+    let [allUsers, setAllUsers] = useState()
+
+    var userSignupData=JSON.parse(sessionStorage.getItem('userSignupData'))
+
+    useEffect(() => {
+        axios.get('https://gmail-clone-be-zsgo.onrender.com')
+            .then((response) => {
+                setAllUsers(response.data)
+            });
+    }, [allUsers])
+
+    function nextClick() {
+        const mobile = document.getElementById('mobile')
+        const mobileError = document.getElementById('mobileError')
+        if (mobile.value === '') {
+            mobileError.innerHTML = `<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Required`
+        }
+        else {
+            if (isNaN(mobile.value)) {
+                mobileError.innerHTML = `<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Invalid`
+            }
+            else if (mobile.value.length < 10 || mobile.value.length > 10) {
+                mobileError.innerHTML = `<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Invalid`
+            }
+            else {
+                userSignupData.mobile=mobile.value
+                sessionStorage.setItem('userSignupData', JSON.stringify(userSignupData))
+                navigate('/mobile-verify')
+            }
+        }
+    }
+
+    function mobileValidate() {
+        const mobile = document.getElementById('mobile')
+        const mobileError = document.getElementById('mobileError')
+        if (mobile.value === '') {
+            mobileError.innerHTML = `<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Required`
+        }
+        else if (isNaN(mobile.value)) {
+            mobileError.innerHTML = `<i class="fa-sharp fa-solid fa-circle-exclamation"></i> Invalid`
+        }
+        else {
+            mobileError.innerHTML = ``
+        }
+    }
+    return (
+        <>
+            <div className="container col-7 border border-secondary rounded position-absolute top-50 start-50 translate-middle">
+                <div className='m-4'>
+                    <div className='row'>
+                        <div className="col-6">
+                            <div>
+                                <h3 className='mb-4'>Google</h3>
+                                <p className="fs-4 mb-4">Verify your phone number</p>
+                                <p className="fs-6">For your security, Google wants to make sure it’s really you. Google will send a text message with a verification code.</p>
+                            </div>
+                            <div style={{ marginTop: '40px' }}>
+                                <TextField label="Phone number" id="mobile" size="small" onKeyUp={() => { mobileValidate() }} />
+                                <p id='mobileError' className='text-danger'></p>
+                            </div>
+                            <div className='d-flex mt-5 justify-content-between' >
+                                <div >
+                                    <Button variant="text" href="/signup">Back</Button>
+                                </div>
+                                <div>
+                                    <button className='btn btn-primary' onClick={() => { nextClick() }}>Next</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col position-absolute top-50 end-0 translate-middle-y text-center pb-5" style={{ width: '320px', height: '320px', marginRight: '60px' }}>
+                            <img src="https://ssl.gstatic.com/accounts/signup/glif/personal.svg" alt="" style={{ width: '100%', height: '100%' }} />
+                            <p className='m-0'>Your personal info is private & safe</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SignupPhoneNumber
