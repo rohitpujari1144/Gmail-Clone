34be80612c25f87a794b518bddb5c13cabb80c6a
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-07-09 00:55:10 +0530

    Update index.js

--- Backend/Email Backend/index.js
+++ Backend/Email Backend/index.js
@@ -5,8 +5,9 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+const dbUrl = 'mongodb+srv://rohit10231:rohitkaranpujari@cluster0.kjynvxt.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(dbUrl)
-const port = 6000
+const port = 6500
 
 // getting all emails from all emails collection
 app.get('/', async (req, res) => {
@@ -25,13 +26,18 @@ app.get('/', async (req, res) => {
     }
 })
 
-// getting all emails of a particular user
-app.get('/getEmails/:userEmail', async (req, res) => {
+// getting all emails of a user
+app.get('/getEmails/:email', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Gmail_Clone')
-        let usersEmails = await db.collection('All Emails').find({emailTo:req.params.userEmail}).toArray()
-        res.status(200).send(usersEmails)
+        let usersEmails = await db.collection('All Emails').aggregate([{ $match: { emailTo: req.params.email } }]).toArray()
+        if (usersEmails.length !== 0) {
+            res.status(200).send(usersEmails)
+        }
+        else {
+            res.send({ message: 'No emails found' })
+        }
     }
     catch (error) {
         console.log(error);
@@ -42,13 +48,13 @@ app.get('/getEmails/:userEmail', async (req, res) => {
     }
 })
 
-// sending new email to All Emails collection
+// sending new email to user
 app.post('/newEmail', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Gmail_Clone')
         await db.collection('All Emails').insertOne(req.body)
-        res.status(201).send({ message: 'New Email sent to all emails collection', data: req.body })
+        res.status(201).send({ message: 'email sent', data: req.body })
     }
     catch (error) {
         console.log(error);
@@ -59,30 +65,13 @@ app.post('/newEmail', async (req, res) => {
     }
 })
 
-// updating emails in All Emails collection
-app.put('/updateEmailInfo/:emailId', async (req, res) => {
+// updating email status
+app.put('/updateEmailInfo/:id', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Gmail_Clone')
-        await db.collection('All Emails').updateOne({ _id: mongodb.ObjectId(req.params.emailId) }, { $set: req.body })
-        res.status(201).send({ message: 'Email updated', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// getting all spam emails
-app.get('/allSpamEmails', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let allSpamEmails = await db.collection('Spam Emails').find().toArray()
-        res.status(200).send(allSpamEmails)
+        await db.collection('All Emails').updateOne({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+        res.status(201).send({ message: 'Email status updated', data: req.body })
     }
     catch (error) {
         console.log(error);
@@ -94,11 +83,11 @@ app.get('/allSpamEmails', async (req, res) => {
 })
 
 // deleteing all spam emails in one click
-app.delete('/emptySpam', async (req, res) => {
+app.delete('/deleteAllSpamEmails/:email', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Gmail_Clone')
-        let removedEmails = await db.collection('All Emails').deleteMany({ spam:true })
+        let removedEmails = await db.collection('All Emails').deleteMany({ spam: true, emailTo: req.params.email })
         res.status(200).send({ message: 'All spam emails deleted successfully', data: removedEmails })
     }
     catch (error) {
@@ -110,218 +99,13 @@ app.delete('/emptySpam', async (req, res) => {
     }
 })
 
-// sending email to spam emails collection
-app.post('/spamEmail', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        await db.collection('Spam Emails').insertOne(req.body)
-        res.status(201).send({ message: 'Email sent to spam emails collection', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// deleteing particular from spam emails collection
-app.delete('/deleteSpamEmail/:emailId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let removedSpamEmail = await db.collection('Spam Emails').deleteOne({ _id: mongodb.ObjectId(req.params.emailId) })
-        res.status(200).send({ message: 'Spam Email deleted successfully', data: removedSpamEmail })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// deleting email from all Emails collection
-app.delete('/deleteEmail/:emailId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let removedEmail = await db.collection('All Emails').deleteOne({ _id: mongodb.ObjectId(req.params.emailId) })
-        res.status(200).send({ message: 'Email deleted successfully', data: removedEmail })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// getting all trash emails
-app.get('/allTrashEmails', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let allTrashEmails = await db.collection('Trash Emails').find().toArray()
-        res.status(200).send(allTrashEmails)
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
 // deleting all trash emails in one click
-app.delete('/emptyTrash', async (req, res) => {
+app.delete('/deleteAllTrashEmails/:email', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
         const db = await client.db('Gmail_Clone')
-        let removedEmails = await db.collection('All Emails').deleteMany({ isTrash:true })
+        let removedEmails = await db.collection('All Emails').deleteMany({ trash: true, emailTo: req.params.email })
         res.status(200).send({ message: 'All trash emails deleted successfully', data: removedEmails })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// sending email to trash
-app.post('/trashEmail', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        await db.collection('Trash Emails').insertOne(req.body)
-        res.status(201).send({ message: 'Email added into trash emails collection', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// send email to trash after clicking delete email
-app.put('/trashEmail/:emailObjectId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        await db.collection('All Emails').findOneAndReplace({_id:mongodb.ObjectId(req.params.emailObjectId)}, {$set:{new:req.body}})
-        res.status(201).send({ message: 'Email updated as trash email', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-
-// deleting email from trash email collection after sending it inbox
-app.delete('/deleteTrashEmail/:emailId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let deletedTrashEmail = await db.collection('Trash Emails').deleteOne({ _id: mongodb.ObjectId(req.params.emailId) })
-        res.status(200).send({ message: 'Email deleted successfully', data: deletedTrashEmail })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// getting all sent emails of a particlar user from All Emails collection
-app.get('/allSentEmails/:emailFrom', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let allSentEmails = await db.collection('All Emails').find({isSent:true, emailFrom:req.params.emailFrom} ).toArray()
-        res.status(200).send(allSentEmails)
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// updating sent email details in All Emails collection
-app.put('/updateSentEmail/:emailObjectId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        await db.collection('All Emails').findOneAndUpdate({_id:mongodb.ObjectId(req.params.emailObjectId)}, {$set:req.body})
-        res.status(201).send({ message: 'Email updated', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// deleting sent email from All Emails collection 
-app.delete('/deleteSentEmail/:emailObjectId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        await db.collection('All Emails').findOneAndDelete({_id:mongodb.ObjectId(req.params.emailObjectId)})
-        res.status(201).send({ message: 'Email deleted', data: req.body })
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// getting all Draft Emails of a particular user
-app.get('/getAllDraftEmails/:emailFrom', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let getAllDraftEmails = await db.collection('All Emails').find({isDraftEmail:true, emailFrom:req.params.emailFrom} ).toArray()
-        res.status(200).send(getAllDraftEmails)
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal server error', error })
-    }
-    finally {
-        client.close()
-    }
-})
-
-// deleteing particular draft email from All Emails collection
-app.delete('/deleteDraftEmail/:emailId', async (req, res) => {
-    const client = await MongoClient.connect(dbUrl)
-    try {
-        const db = await client.db('Gmail_Clone')
-        let removedDraftEmail = await db.collection('All Emails').deleteOne({ _id: mongodb.ObjectId(req.params.emailId) })
-        res.status(200).send({ message: 'Draft Email deleted successfully', data: removedDraftEmail })
     }
     catch (error) {
         console.log(error);
